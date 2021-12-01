699abd044e7a82c410d245ce24f0dabe2d145486
Matthew <matthew.d.baker20@gmail.com>
2021-12-01 17:57:28 -0600

    refactor 2021

---
+++ 2015/day-03/solve.js
@@ -0,0 +1,17 @@
+read('data.txt', solve).then(console.log);
+
+function solve(data) {
+    data = data[0];
+
+    console.log(data);
+    // create an array structure and fill it with the position you are at kinda like this im thinking
+    /*       s, then v, <, v, >, >, ^, ^, >, v, >
+    * [0, 0, 1, 1, 1, 0]
+    * [0, 1, 1, 1, 1, 1]
+    * [0, 1, 1, 1, 0, 0]
+    * */
+    for(let i = 0; i < data.length; i++) {
+
+    }
+
+}
--- 2021/day-01/solve.js
+++ 2021/day-01/solve.js
@@ -3,7 +3,7 @@
 read('data.txt', solve2).then(console.log);
 
 function solve1(data) {
-    console.log(data);
+    // console.log(data);
     let increaseCount = 0;
     for(let i = 1; i < data.length; i++) {
         let curr = parseInt(data[i]);
@@ -17,8 +17,8 @@ function solve2(data) {
     let count = 0;
     data = data.map(x => parseInt(x));
     for(let i = 0; i < data.length - 3; i++) {
-        let g1 = [data[i], data[i + 1], data[i + 2]].reduce((x, y) => x + y);
-        let g2 = [data[i + 1], data[i + 2], data[i + 3]].reduce((x, y) => x + y);
+        let g1 = data.slice(i, i + 3).reduce((x, y) => x + y);
+        let g2 = data.slice(i + 1, i + 4).reduce((x, y) => x + y);
         count += g2 > g1 ? 1 : 0;
     }
     return count;
